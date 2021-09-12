ef806aec49b30faa77f755dd818974841f857c61
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2021-09-12 15:55:04 -0500

    Update

--- src/components/contenido.vue
+++ src/components/contenido.vue
@@ -5,6 +5,7 @@
           <div>
             <video controls width="80%">
               <source src="../assets/multimedia/1.mp4" type="video/mp4">
+              
             </video>
           </div>
          </v-col>
