53e5ca26adc44a2aa68714d1dd77736598b4f854
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2021-09-12 17:54:35 -0500

    UPDATE

--- src/router/index.js
+++ src/router/index.js
@@ -1,68 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Inicio from '../components/Inicio.vue'
-import contenido from "../components/contenido.vue";
-import creditos from "../components/creditos.vue";
-import evaluacion from "../components/evaluacion.vue";
-import actividades from"../components/actividades.vue";
-import { _ } from 'core-js/es5';
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'inicio',
-    component: Inicio
- },
-{
-    path: '/entrada',
-    name: 'entrada',
-    component : entrada,
-   
-    children: [
-     {
-      path: '/contenido',
-      name: 'contenido',
-      component: contenido,
-     },
-     {
-      path: '/actividades',
-      name: 'actividades',
-      component: actividades,
-      children: [
-
-      {
-           path: 'acti1',
-           name: 'acti1',
-           component: acti1,
-        },
-        {
-          path: 'acti2',
-          name: 'acti2',
-          component: acti2,
-       }
-    ],
-    },
-    {
-      path: '/evaluacion',
-      name: 'evaluacion',
-      component: evaluacion,
-      children: [
-        {
-           path: 'eva',
-           name: 'eva',
-           component: eva,
-        },
-
-    ],
-      },
-    {
-     path: '/creditos',
-     name: 'creditos',
-     component: creditos,
-     },
-    ],
-}
+    name: 'Inicio',
+    component: () => import("../components/Inicio.vue") 
+  },
+  {
+    path: "/siguiente",
+    name: "NuevoC",
+    component: () => import('../views/ComponenteNuevo.vue')
+  },
+  {
+    path: "/contenido",
+    name: "Contenido",
+    component: () => import("../views/contenido.vue")
+  },
+  {
+    path: "/evaluacion",
+    name: "Evaluacion",
+    component: () => import("../views/evaluacion.vue")
+  },
+  {
+    path: "/creditos",
+    name: "Creditos",
+    component: () => import("../views/credito.vue")
+  }
 ]
 
 const router = new VueRouter({
