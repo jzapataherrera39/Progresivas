c67ccc280935d1b1a4b3d45b7ba0ad0737f0fc59
jzapataherrera39 <brayansaid06@gmail.com>
2021-09-12 19:20:48 -0500

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,72 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Inicio from '../components/Inicio'
+import contenido from "../components/contenido";
+import creditos from "../components/creditos";
+import entrada from "../components/entrada.vue";
+import acti1 from "../components/h5p/actividad1";
+import acti2 from "../components/h5p/actividad2";
+import actividades from"../components/actividades";
+import evaluacion from"../components/evaluacion";
+import eva from "../components/h5p/evaluacion";
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Inicio',
-    component: () => import("../components/Inicio.vue") 
+     path: '/',
+     name: 'inicio',
+     component: Inicio
   },
-  {
-    path: "/siguiente",
-    name: "actividades",
-    component: () => import('../components/Inicio.vue')
-  },
-  {
-    path: "/contenido",
-    name: "contenido",
-    component: () => import("../components/contenido.vue")
-  },
-  {
-    path: "/evaluacion",
-    name: "evaluacion",
-    component: () => import("../components/evaluacion.vue")
-  },
-  {
-    path: "/creditos",
-    name: "creditos",
-    component: () => import("../components/creditos.vue")
-  }
-]
+ {
+     path: '/entrada',
+     name: 'entrada',
+     component : entrada,
+    
+     children: [
+      {
+       path: '/contenido',
+       name: 'contenido',
+       component: contenido,
+      },
+      {
+       path: '/actividades',
+       name: 'actividades',
+       component: actividades,
+       children: [
 
+       {
+            path: 'acti1',
+            name: 'acti1',
+            component: acti1,
+         },
+         {
+           path: 'acti2',
+           name: 'acti2',
+           component: acti2,
+        }
+     ],
+     },
+     {
+       path: '/evaluacion',
+       name: 'evaluacion',
+       component: evaluacion,
+       children: [
+         {
+            path: 'eva',
+            name: 'eva',
+            component: eva,
+         },
+
+     ],
+       },
+     {
+      path: '/creditos',
+      name: 'creditos',
+      component: creditos,
+      },
+     ],
+ }
+]
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
